9eb38397782456bbbde12af585292b51b52095af
Rohan Kumar Mahto <rohankumar993985@gmail.com>
2025-12-26 16:12:24 +0530

    fixed cohere new version issues

--- src/api/cohere.ts
+++ src/api/cohere.ts
@@ -3,29 +3,52 @@ export const fetchCohereResponse = async (
   botRole: string
 ): Promise<string> => {
   try {
-    const response = await fetch("https://api.cohere.ai/v1/chat", {
+    const response = await fetch("https://api.cohere.com/v2/chat", {
       method: "POST",
       headers: {
         Authorization: `Bearer ${import.meta.env.VITE_COHERE_API_KEY}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "command-r-plus",
-        message: userMessage,
-        chat_history: [],
-        connectors: [],
+        model: "command-a-reasoning-08-2025",
+        messages: [
+          {
+            role: "system",
+            content: [
+              {
+                type: "text",
+                text: botRole,
+              },
+            ],
+          },
+          {
+            role: "user",
+            content: [
+              {
+                type: "text",
+                text: userMessage,
+              },
+            ],
+          },
+        ],
         temperature: 0.3,
-        prompt_truncation: "auto",
-        preamble: botRole,
       }),
     });
 
-    const data = await response.json();
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error("Cohere API error:", errorText);
+      return "Cohere API error";
+    }
 
-    return data?.text || "No response received.";
+    const data = await response.json();
+    console.log(data)
+    return (
+      data?.message?.content?.[1]?.text ||
+      "No response received."
+    );
   } catch (error) {
     console.error("Cohere API error:", error);
-        return "Something went wrong, Please try again later";
-
+    return "Something went wrong, please try again later";
   }
 };
